db9aefed11c0d0e7658f9db78a74cad27177797c
Philip Durbin <philip_durbin@harvard.edu>
2024-10-07 21:43:42 -0400

    change to Hello Dataverse!

--- app/src/main/java/org/dataverse/android/dataverse/MainActivity.kt
+++ app/src/main/java/org/dataverse/android/dataverse/MainActivity.kt
@@ -42,6 +42,6 @@ fun Greeting(name: String, modifier: Modifier = Modifier) {
 @Composable
 fun GreetingPreview() {
     DataverseTheme {
-        Greeting("Android")
+        Greeting("Dataverse")
     }
 }
