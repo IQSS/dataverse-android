8a92d4392a5717fc5e413e8f81b835c203ed61b6
Philip Durbin <philip_durbin@harvard.edu>
2024-10-08 19:28:19 -0400

    put Android back

--- app/src/main/java/org/dataverse/android/dataverse/MainActivity.kt
+++ app/src/main/java/org/dataverse/android/dataverse/MainActivity.kt
@@ -21,7 +21,7 @@ class MainActivity : ComponentActivity() {
             DataverseTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
                     Greeting(
-                        name = "Dataverse",
+                        name = "Android",
                         modifier = Modifier.padding(innerPadding)
                     )
                 }
